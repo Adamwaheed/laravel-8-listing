e2b5ad7fafc57ae984c200a6cd220b980045cfaf
Adam <i@live.mv>
2021-02-08 22:18:54 +0500

    menu make great again

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">Inventory</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -12,13 +12,13 @@
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="{{route('items.index')}}"><i class="fab fa-buffer"></i>Item</a>
+            <a class="nav-link" href="{{route('items.index')}}"><i class="fab fa-buffer"></i> Item</a>
           </li>
     
         </ul>
         <form class="d-flex" action="" method="get">
           <input class="form-control me-2" name='search' type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <button class="btn btn-primary" type="submit">Search</button>
         </form>
       </div>
     </div>
